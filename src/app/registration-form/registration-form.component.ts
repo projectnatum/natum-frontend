1de5f9883983300b8c607bcc937b7e778587a789
snigdhesh <snigdhesh@live.com>
2019-06-25 09:49:01 -0400

    adding registration form

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -40,9 +40,10 @@ export class RegistrationFormComponent implements OnInit {
     // console.log(obj,"--->converted date");
     let registrationData=Object.assign({},this.registrationForm.value);
     this.registrationService.saveCustomers(registrationData).subscribe(
-      (res)=>console.log(res,"from server")
+      (res)=>console.log(res._body,"from server")
     )   
-
+    this.registrationForm.reset();
+     
   }
 
 }
